c14e5990db7789d549a1571612ebca1a6552246b
Abhimanyu Kumar Chauhan <ce367@CE-367.local>
2020-05-23 16:33:02 +0530

    Project code to share

--- App/Store/Reduces/BuyItemsReducer.js
+++ App/Store/Reduces/BuyItemsReducer.js
@@ -6,24 +6,27 @@ const initialIphoneState = {
 }
 
 const reducer = (state = initialIphoneState, action) => {
-    let item = JsonData[action.payload - 1]
-     
+    //JsonData is a local data, we can take this from redux state too
+    //state.data and we can midify that item instead
+    let item = JsonData.find(item => {
+        return item.id == action.payload
+    })
     switch (action.type) {
         case INCREMENT:
             item.numberOfItems = item.numberOfItems + 1
             return {
                 ...state,
-                JsonData: JsonData
+                JsonData: [...JsonData]
             }
 
         case DECREMENT:
-            if(item.numberOfItems !== 0){
+            if (item.numberOfItems !== 0) {
                 item.numberOfItems = item.numberOfItems - 1
             }
-            
+
             return {
                 ...state,
-                JsonData: JsonData
+                JsonData: [...JsonData]
             }
         default: return state
     }
